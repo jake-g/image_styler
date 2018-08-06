b70e09e085768f7bea2655b665d4cfa52b0972df
jake-g <omonoid@uw.edu>
2018-08-05 21:04:18 -0700

    make package for pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='neural_image_styler',
+      version='0.1',
+      description='apply neural styling to an input image',
+      url='http://github.com/jake-g/image_styler',
+      author='jake-g',
+      author_email='omonoid@gmail.com',
+      license='MIT',
+      packages=['neural_image_styler'],
+      zip_safe=False)
