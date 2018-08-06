fa169d5ad98d4a8b8edb972ab43ead0963c818d6
jake-g <omonoid@uw.edu>
2018-08-05 21:12:37 -0700

    make package for pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='neural_image_styler',
       version='0.1',
       description='apply neural styling to an input image',
-      url='http://github.com/jake-g/image_styler',
+      url='http://github.com/jake-g/neural_image_styler',
       author='jake-g',
       author_email='omonoid@gmail.com',
       license='MIT',
